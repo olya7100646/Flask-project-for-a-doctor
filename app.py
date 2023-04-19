cdbe0fac6bb0136201aa333e0ff62737dfc04e17
Ольга Леонович <115556248+olya7100646@users.noreply.github.com>
2023-04-19 09:36:57 +0300

    Небольшой сайт для врача психиатра-психотерапевта
    В основу сайта заложено написание небольших текстов врачом-психиатром-психотерапевтом. а также информация о специалисте и контактная информация.

---
+++ app.py
@@ -0,0 +1,102 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Article %r>' % self.id
+
+
+@app.route('/')
+@app.route('/home')
+def index():  # put application's code here
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
+@app.route('/posts')
+def posts():  # put application's code here
+    articles = Article.query.order_by(Article.date.desc()).all()
+    return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def posts_detail(id):  # put application's code here
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def posts_delete(id):  # put application's code here
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "При удалении статьи произошла ошибка"
+
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):  # put application's code here
+    article = Article.query.get(id)
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        article = Article.query.get(id)
+        return render_template("post_update.htm", article=article)
+
+
+@app.route('/create-article', methods=['POST', 'GET'])
+def create_article():  # put application's code here
+    if request.method == "POST":
+        title = request.form['title']
+        intro = request.form['intro']
+        text = request.form['text']
+
+        article = Article(title=title, intro=intro, text=text)
+        try:
+            db.session.add(article)
+            db.session.commit()
+            return redirect('/posts')
+
+        except:
+            return "При добавлении статьи произошла ошибка"
+    else:
+        return render_template("create-article.html")
+
+
+@app.route('/contact')
+def contacts():  # put application's code here
+    return render_template('contacts.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
